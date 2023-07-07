23177c619adf206ef2fb27756cd199434f426654
franck <badinisharif23@gmail.com>
2023-07-08 00:43:11 +0800

    sqlachemy and sql setup

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root@localhost/flask_blog"
+app.config['SECRET_KEY'] = "thesecretkeywhichissupposedtobesecret"
+db = SQLAlchemy(app)
+admin = Admin(app)
 
-@app.route('/')
+# DATABASE CLASS
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    date = db.Column(db.DateTime)
+    slug = db.Column(db.String(255))
+
+with app.app_context():
+    db.create_all()
+
+admin.add_view(ModelView(Posts, db.session))
+
+@app.route("/")
 def homepage():
-    return render_template("homepage.html")
+    posts = Posts.query.all()
+    return render_template("homepage.html", posts=posts)
 
-@app.route("/post/<int:number>")
-def post_page(number):
-    return render_template("post.html")
+@app.route("/post/<string:slug>")
+def post_page(slug):
+    try:
+        post = Posts.query.filter_by(slug=slug).one()
+        return render_template("post.html", post=post)
+    except:
+        abort(404)
+
 
 @app.route("/about")
 def about():
@@ -18,17 +46,6 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/admin")
-def login():
-    return render_template("admin.html")
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-@app.route("/new_post")
-def create_post():
-    return render_template("create_post.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
